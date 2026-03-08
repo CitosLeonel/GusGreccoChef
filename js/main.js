09c65d7e904ad034e03a24522a83a8b8869ad2f5
Leonel Martin <leo.mezanzano@live.com.ar>
2026-03-08 13:09:04 -0300

    incorporando galleria al index

--- js/main.js
+++ js/main.js
@@ -19,18 +19,4 @@ document.addEventListener("DOMContentLoaded", function () {
       locale: "en",
     });
   }
-
-  // ---- Swiper JS ---- //
-  const swiper = new Swiper(".swiper", {
-    // Parámetros opcionales
-    direction: "horizontal",
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
 });
